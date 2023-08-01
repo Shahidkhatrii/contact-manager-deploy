2443e63017d608093a3e96ac3eb5eec913310d46
shahidkhatrii <shahidkhatrixyz@gmail.com>
2023-08-01 13:23:30 +0530

    setting origin

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://contacts-manager-afev.onrender.com/",
+    origin: "https://contacts-manager-afev.onrender.com/*",
   })
 );
 app.use("/api/contacts", require("./routes/contactRoutes"));
